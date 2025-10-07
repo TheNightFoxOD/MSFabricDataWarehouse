af4561f3282cb85a5882d1428abf908470fd8802
Leonard Chesca (EXT) <leonardc@od.org>
2025-10-07 14:04:58 +0000

    Manual Metadata Seed Notebook seed json update

---
+++ Various test scripts/Schema check scripts.Notebook/notebook-content.py
@@ -0,0 +1,49 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "4aee8a32-be91-489f-89f3-1a819b188807",
+# META       "default_lakehouse_name": "Master_Bronze",
+# META       "default_lakehouse_workspace_id": "b0f83c07-a701-49bb-a165-e06ca0ee4000",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "4aee8a32-be91-489f-89f3-1a819b188807"
+# META         },
+# META         {
+# META           "id": "234e6789-2254-455f-b2b2-36d881cb1c17"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC describe TABLE Dataverse_Master_Staging.account
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sql
+# MAGIC describe TABLE  Master_Bronze.dataverse.account
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
